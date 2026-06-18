aab3d9e603b407e3773bb3a49b957f20030e22f6
RainGoal <88344822@qq.com>
2026-06-18 14:34:35 +0800

    feat:ui优化

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "money-ledger-v46";
+const CACHE_NAME = "money-ledger-v47";
 const BASE_PATH = "__BASE_PATH__";
-const ASSET_VERSION = "46";
+const ASSET_VERSION = "47";
 const ASSETS = [
   `${BASE_PATH}/`,
   `${BASE_PATH}/index.html`,
