9da6a6ac7631054440b7bad1dda6bd316d04093f
RainGoal <88344822@qq.com>
2026-06-23 14:38:37 +0800

    Constrain dashboard month picker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "money-ledger-v57";
+const CACHE_NAME = "money-ledger-v58";
 const BASE_PATH = "__BASE_PATH__";
-const ASSET_VERSION = "57";
+const ASSET_VERSION = "58";
 const ASSETS = [
   `${BASE_PATH}/`,
   `${BASE_PATH}/index.html`,
